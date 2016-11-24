d9c63518fb357e6cb0c59868fb541fbbbf8becce
fabiodoo <fabiocarvalho.mail@gmail.com>
2016-11-24 17:29:31 +0100

    kick off all

---
+++ MyFirstAzureDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFirstAzureDemo.Startup))]
+namespace MyFirstAzureDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
